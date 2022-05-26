61d402a73ee6eca402602b9946aa2b7a210fd2e7
Andrew Carr <andrew@foxglove.dev>
2022-05-26 11:59:00 +1000

    Change zooming method to arethmeatic increase and remove input

--- packages/studio-base/src/panels/Image/components/ZoomMenu.tsx
+++ packages/studio-base/src/panels/Image/components/ZoomMenu.tsx
@@ -3,8 +3,8 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import { Button, Menu, MenuItem, TextField, styled as muiStyled, Divider } from "@mui/material";
-import { useCallback, useEffect, useState } from "react";
+import { Button, Menu, MenuItem, styled as muiStyled, Divider } from "@mui/material";
+import { useState } from "react";
 
 import { Config } from "../types";
 
@@ -24,15 +24,6 @@ const StyledButton = muiStyled(Button)(({ theme }) => ({
   },
 }));
 
-const StyledMenuItem = muiStyled(MenuItem)(({ theme }) => ({
-  padding: theme.spacing(1),
-  margin: theme.spacing(-1, 0),
-
-  "&:hover, &:focus-within": {
-    backgroundColor: "transparent",
-  },
-}));
-
 export function ZoomMenu({
   zoom,
   setPanZoom,
@@ -42,7 +33,6 @@ export function ZoomMenu({
 }): JSX.Element {
   const zoomPercentage = `${Math.round(100 * zoom)}%`;
 
-  const [localZoom, setLocalZoom] = useState(zoomPercentage);
   const [anchorEl, setAnchorEl] = useState<undefined | HTMLElement>(undefined);
 
   const open = Boolean(anchorEl);
@@ -55,19 +45,6 @@ export function ZoomMenu({
     setAnchorEl(undefined);
   };
 
-  useEffect(() => {
-    setLocalZoom(zoomPercentage);
-  }, [zoomPercentage]);
-
-  const onLocalZoomKeyDown = useCallback(
-    (event: React.KeyboardEvent) => {
-      if (event.key === "Enter") {
-        setPanZoom({ zoom: parseFloat(localZoom) / 100 });
-      }
-    },
-    [localZoom, setPanZoom],
-  );
-
   return (
     <>
       <StyledButton
@@ -76,7 +53,7 @@ export function ZoomMenu({
         color="inherit"
         id="zoom-button"
         variant="text"
-        aria-controls={open ? "basic-menu" : undefined}
+        aria-controls={open ? "zoom-menu" : undefined}
         aria-haspopup="true"
         aria-expanded={open ? "true" : undefined}
         onClick={handleClick}
@@ -93,24 +70,16 @@ export function ZoomMenu({
           "aria-labelledby": "zoom-button",
         }}
       >
-        <StyledMenuItem>
-          <TextField
-            autoFocus={open}
-            onKeyDown={onLocalZoomKeyDown}
-            onChange={(event) => setLocalZoom(event.target.value)}
-            value={localZoom}
-            size="small"
-          />
-        </StyledMenuItem>
+        <MenuItem onClick={() => setPanZoom({ zoom: zoom + 1 * 0.5 })}>Zoom in</MenuItem>
+        <MenuItem onClick={() => setPanZoom({ zoom: zoom - 1 * 0.5 })} disabled={zoom <= 0.5}>
+          Zoom out
+        </MenuItem>
         <Divider />
-        <MenuItem onClick={() => setPanZoom({ zoom: zoom * 1.1 })}>Zoom in</MenuItem>
-        <MenuItem onClick={() => setPanZoom({ zoom: zoom * 0.9 })}>Zoom out</MenuItem>
-        <Divider />
-        <MenuItem onClick={() => setPanZoom({ zoom: 1, pan: { x: 0, y: 0 } })}>
+        <MenuItem onClick={() => setPanZoom({ zoom: 1, mode: "other", pan: { x: 0, y: 0 } })}>
           Zoom to 100%
         </MenuItem>
         <MenuItem onClick={() => setPanZoom({ zoom: 1, mode: "fit", pan: { x: 0, y: 0 } })}>
-          Zoom to fit
+          Zoom to fill
         </MenuItem>
         <MenuItem onClick={() => setPanZoom({ zoom: 1, mode: "fill", pan: { x: 0, y: 0 } })}>
           Zoom to fill
