742ff4dcdc2a9ef6b30d6cf3a9044af4b1f3ffe7
John Hurliman <jhurliman@jhurliman.org>
2022-08-02 11:37:14 +0800

    Benchmark generator script

---
+++ benchmark/generator/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CONTAINER="studio-benchmark-generator:1"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd ${SCRIPT_DIR}
+
+echo "Building ${CONTAINER}"
+docker build -t ${CONTAINER} .
+
+echo "Running ${CONTAINER}"
+docker run --rm -p 8888:8888 -v `pwd`:/notebooks ${CONTAINER}
