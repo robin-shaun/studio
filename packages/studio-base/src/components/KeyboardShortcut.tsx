656761a92755f954f7a3c95628a9471409a7c732
Andrew Carr <andrew@foxglove.dev>
2022-09-06 10:51:40 +1000

    Refactor KeyboardShortcut to remove muiStyled

--- packages/studio-base/src/components/KeyboardShortcut.tsx
+++ packages/studio-base/src/components/KeyboardShortcut.tsx
@@ -1,82 +1,62 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
-//
-// This file incorporates work covered by the following copyright and
-// permission notice:
-//
-//   Copyright 2018-2021 Cruise LLC
-//
-//   This source code is licensed under the Apache License, Version 2.0,
-//   found at http://www.apache.org/licenses/LICENSE-2.0
-//   You may not use this file except in compliance with the License.
 
-import { styled as muiStyled } from "@mui/material";
+import { makeStyles } from "tss-react/mui";
 
 import { fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
 
-const SKeyboardShortcut = muiStyled("div")(({ theme }) => ({
-  padding: theme.spacing(0.5, 0),
-  maxWidth: 400,
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-}));
+const useStyles = makeStyles()((theme) => ({
+  key: {
+    padding: theme.spacing(0, 0.5),
+    lineHeight: 1.5,
+    fontFamily: fonts.SANS_SERIF,
+    textAlign: "center",
+    border: "1px solid currentColor",
+    borderRadius: theme.shape.borderRadius,
+    fontSize: theme.typography.body2.fontSize,
+    fontWeight: 500,
+    minWidth: 20,
 
-const SDescription = muiStyled("div")(({ theme }) => ({
-  marginRight: theme.spacing(2),
-}));
-
-const SKeyWrapper = muiStyled("div")(({ theme }) => ({
-  display: "inline-flex",
-  flex: "none",
-  color: theme.palette.text.secondary,
-  border: `1px solid ${theme.palette.text.secondary}`,
-  borderRadius: theme.shape.borderRadius,
-  fontSize: theme.typography.body2.fontSize,
-  fontWeight: 500,
-  minWidth: 20,
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const SKey = muiStyled("kbd")(({ theme }) => ({
-  padding: theme.spacing(0, 0.5),
-  lineHeight: 1.5,
-  fontFamily: fonts.SANS_SERIF,
-
-  "&:not(:last-child)": {
-    borderRight: `1px solid ${theme.palette.text.secondary}`,
+    "&:not(:last-child)": {
+      borderRight: "1px solid currentColor",
+    },
+  },
+  keys: {
+    color: theme.palette.text.disabled,
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: "none",
+    gap: theme.spacing(0.5),
+    flexWrap: "wrap",
+  },
+  root: {
+    margin: theme.spacing(1, 0),
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: theme.spacing(1),
   },
 }));
 
 type Props = {
   keys: string[];
   description?: string;
-  descriptionMaxWidth?: number;
 };
 
-export default function KeyboardShortcut({
-  keys,
-  description,
-  descriptionMaxWidth,
-}: Props): JSX.Element {
+export default function KeyboardShortcut({ keys, description }: Props): JSX.Element {
+  const { classes } = useStyles();
   return (
-    <SKeyboardShortcut>
-      {description != undefined && description.length > 0 && (
-        <SDescription
-          style={descriptionMaxWidth != undefined ? { width: descriptionMaxWidth } : {}}
-        >
-          {description}
-        </SDescription>
-      )}
-      <span>
-        {keys.map((key, idx) => (
-          <SKeyWrapper key={idx} style={idx < keys.length - 1 ? { marginRight: 4 } : {}}>
-            <SKey>{key}</SKey>
-          </SKeyWrapper>
+    <div className={classes.root}>
+      {description != undefined && description}
+      <div className={classes.keys}>
+        {keys.map((key) => (
+          <kbd key={key} className={classes.key}>
+            {key}
+          </kbd>
         ))}
-      </span>
-    </SKeyboardShortcut>
+      </div>
+    </div>
   );
 }
