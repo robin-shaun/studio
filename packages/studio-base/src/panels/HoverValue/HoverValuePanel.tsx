5f23ed55935ef305892309661b2fbedad23428d7
Andrew Carr <andrew@foxglove.dev>
2022-09-16 08:12:32 +1000

    Move styling into makeStyles

--- packages/studio-base/src/panels/HoverValue/HoverValuePanel.tsx
+++ packages/studio-base/src/panels/HoverValue/HoverValuePanel.tsx
@@ -2,19 +2,50 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import RemoveIcon from "@mui/icons-material/Remove";
 import { IconButton } from "@mui/material";
 import { useLayoutEffect, useState } from "react";
+import { makeStyles } from "tss-react/mui";
 
 import { PanelExtensionContext } from "@foxglove/studio";
-import Stack from "@foxglove/studio-base/components/Stack";
 import ThemeProvider from "@foxglove/studio-base/theme/ThemeProvider";
 
 type HoverValuePanelProps = {
   context: PanelExtensionContext;
 };
 
+const useStyles = makeStyles<void, "cell">()((theme, _params, classes) => ({
+  grid: {
+    height: "100%",
+    overflowY: "auto",
+    display: "grid",
+    alignItems: "stretch",
+    gridTemplateColumns:
+      "auto minmax(max-content, 1fr) minmax(max-content, 1fr) minmax(max-content, 1fr)",
+    gridAutoRows: 30,
+    padding: theme.spacing(0.5),
+    rowGap: theme.spacing(0.5),
+  },
+  row: {
+    display: "contents",
+
+    [`&:hover .${classes.cell}`]: {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+  cell: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0.5),
+  },
+  iconButton: {
+    padding: theme.spacing(0.125),
+  },
+}));
+
 export default function HoverValuePanel(props: HoverValuePanelProps): JSX.Element {
   const { context } = props;
+  const { classes } = useStyles();
 
   // setup context render handler and render done handling
   const [renderDone, setRenderDone] = useState<() => void>(() => () => {});
@@ -38,23 +69,27 @@ export default function HoverValuePanel(props: HoverValuePanelProps): JSX.Elemen
 
   return (
     <ThemeProvider isDark={colorScheme === "dark"}>
-      <Stack
-        fullHeight
-        alignItems="center"
-        overflowY="auto"
-        style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr auto" }}
-      >
+      <div className={classes.grid}>
         {Array(10)
           .fill({})
           .map((i, idx) => (
-            <div key={idx} style={{ display: "contents" }}>
-              <div>{idx}</div>
-              <div>key</div>
-              <div>value</div>
-              <IconButton>•••</IconButton>
+            <div key={idx} className={classes.row}>
+              <div className={classes.cell}>
+                <IconButton
+                  className={classes.iconButton}
+                  centerRipple={false}
+                  size="small"
+                  title="Toggle visibility"
+                >
+                  <RemoveIcon color="inherit" />
+                </IconButton>
+              </div>
+              <div className={classes.cell}>messagePath or plot label</div>
+              <div className={classes.cell}>playhead value</div>
+              <div className={classes.cell}>hovered value</div>
             </div>
           ))}
-      </Stack>
+      </div>
     </ThemeProvider>
   );
 }
